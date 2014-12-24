50c98dd4e933f8f62207f215c0c0e97fdded1c4e
needo37 <ezigler@superhero.org>
2014-12-24 10:49:14 -0600

    Update download URL

--- firstrun.sh
+++ firstrun.sh
@@ -15,7 +15,7 @@ if [ "$VERSION" = "$INSTALLED" ]; then
 else
   mv /etc/default/plexmediaserver /tmp/
   apt-get remove --purge -y plexmediaserver
-  wget -P /tmp "http://downloads.plexapp.com/plex-media-server/$VERSION/plexmediaserver_${VERSION}_amd64.deb"
+  wget -P /tmp "http://nightlies.plexapp.com/directdl/plex-media-server/dist-ninja/$VERSION/plexmediaserver_${VERSION}_amd64.deb"
   gdebi -n /tmp/plexmediaserver_${VERSION}_amd64.deb
   mv /tmp/plexmediaserver /etc/default/
 fi
